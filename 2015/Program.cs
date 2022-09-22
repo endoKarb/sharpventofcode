e87440123399586598dfea7b1fc45fa05771a532
Matteo <mrtn.matteo@protonmail.com>
2022-09-23 01:24:33 +0200

    2015 Day1 Part1 Solver

---
+++ 2015/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace _2015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solver = new Day1Solver();
+            Console.WriteLine(solver.Part1());
+        }
+    }
+}
