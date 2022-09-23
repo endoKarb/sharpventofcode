9a953d80e8bdf4773c9bd08fcfa791bd92cec6ed
Matteo <mrtn.matteo@protonmail.com>
2022-09-23 22:20:35 +0200

    Utils: manh dist for coords

--- AoC/Utils.cs
+++ AoC/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,24 @@ namespace AoC
             public override string ToString()
             {
                 return $"x: {X}, y: {Y}";
+            }
+            public static Coords operator -(Coords c1, Coords c2)
+            {
+                return new Coords(c1.X - c2.X, c1.Y - c2.Y);
+            }
+            public static Coords operator +(Coords c1, Coords c2)
+            {
+                return new Coords(c1.X + c2.X, c1.Y + c2.Y);
+            }
+            public static Coords Abs(Coords c1)
+            {
+                return new Coords(x: Math.Abs(c1.X), y: Math.Abs(c1.Y));
+            }
+            public static int ManhDist (Coords c1, Coords c2)
+            {
+                Coords dist = Abs(c1 - c2);
+                //Debug.WriteLine($"{c1}, {c2}, {dist}");
+                return dist.X + dist.Y;
             }
         }
         public enum Direction
