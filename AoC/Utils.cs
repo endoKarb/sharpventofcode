be9f5dc2f8fba3cb92c7e97cec84757fd3fa81a2
Matteo <mrtn.matteo@protonmail.com>
2022-09-23 22:03:11 +0200

    Utils: ToString added to Coords

--- AoC/Utils.cs
+++ AoC/Utils.cs
@@ -8,6 +8,22 @@ namespace AoC
 {
     internal static class Utils
     {
+        public struct Coords
+        {
+            public Coords(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; }
+            public int Y { get; }
+
+            public override string ToString()
+            {
+                return $"x: {X}, y: {Y}";
+            }
+        }
         public enum Direction
         {
             North,
