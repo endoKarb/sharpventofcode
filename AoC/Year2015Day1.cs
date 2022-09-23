72fddd819bfa583227ea0a32930d60fa999e4db6
Matteo <mrtn.matteo@protonmail.com>
2022-09-23 12:37:46 +0200

    2015 day1 part1 solver

--- AoC/Year2015Day1.cs
+++ AoC/Year2015Day1.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AoC
 {
     internal class Year2015Day1 : AoCSolver
     {
+        readonly string _text;
         public Year2015Day1(string filepath)
         {
             _filepath = filepath;
+            _text = File.ReadAllText(filepath);
         }
 
         override public int SolvePart1()
         {
-            return 0;
+            int answer = 0;
+            for (int i = 0; i < _text.Length; i++)
+            {
+                switch (_text[i])
+                {
+                    case '(':
+                        answer++;
+                        break;
+                    case ')':
+                        answer--;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return answer;
         }
 
         override public int SolvePart2()
